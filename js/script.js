97b27a3de7a4be1ce627077fabfe5a1d76b7ad8a
Ren Dela Cruz <ren_delacruz@outlook.com>
2021-05-02 23:48:53 -0400

    Added links

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 document.addEventListener("mousemove", function (e) {
     const card = document.querySelector(".card")
     let cursorX = e.clientX;
-    let cursorY = e.clientY;
+    //let cursorY = e.clientY;
 
     let cardCenterX = card.offsetWidth / 2;
-    let cardCenterY = card.offsetHeight / 2;
+    //let cardCenterY = card.offsetHeight / 2;
 
     card.style.left = (cursorX - cardCenterX) + "px";
-    card.style.top = (cursorY - cardCenterY) + "px";
+    //card.style.top = (cursorY - cardCenterY) + "px";
 });
